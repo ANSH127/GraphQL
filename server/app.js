86da5ff8161520773927178a15864b748f8e7ec0
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-05-07 17:48:25 +0530

    connecting to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const dburl = `mongodb+srv://${process.env.MONOGODB_USERNAME}:${process.env.MONOGODB_PASSWORD}@cluster0.46ihpbt.mongodb.net/?retryWrites=true&w=majority`
 const app = express();
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -12,4 +18,12 @@ app.listen(4000, () => {
     console.log('Server started on port 4000');
 }
 );
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("connected to database");
+}).catch((err) => {
+
+    console.log(err);
+}
+);
+
 
